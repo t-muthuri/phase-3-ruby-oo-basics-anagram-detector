caeb99250d8b691978650d6f63bc97690ef94045
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-10-27 07:34:01 +0300

    Complete assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+#initialize method that takes in a match
+class Anagram
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match (anagram)
+        #use the comparative operator to compare two words/names ==
+        #split the names to letter-strings
+        #use the sort method to compare the letters to find the anagram
+        # anagram.split.sort == word.split.sort
+        
+        anagram.select do |i|
+            string1 = i.split("")
+            string2 = word.split("")
+            string1.sort == string2.sort
+        end
+    end
+end
